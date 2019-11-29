077acb87d2fa0a0ccf481ecbaf599bf0a77122b9
zhouxiang <zhouxiang@peilian.com>
2019-11-29 17:03:00 +0800

    unable auto kafka

--- src/main/java/com/zhou/basespringboot/Application.java
+++ src/main/java/com/zhou/basespringboot/Application.java
@@ -2,13 +2,14 @@ package com.zhou.basespringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
 /**
  * 启动类
  *
  * @author zx
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
